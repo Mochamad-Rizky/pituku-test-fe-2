ab315923b1651af1247b6835434c25eaa9cea38c
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-10-09 23:54:23 +0700

    add: configuration initial project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                hostname: "storage.googleapis.com",
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
